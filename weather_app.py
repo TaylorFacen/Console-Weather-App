c67715559eec5254fd248b84f478d968d19a33d9
Taylor Facen <taylor.facen@gmail.com>
2019-12-11 21:52:24 -0500

    Initial commit

---
+++ weather_app.py
@@ -0,0 +1,20 @@
+import requests
+
+DARK_SKY_API_KEY = "my-super-secret-api-key"
+
+def get_weather(latitude, longitude):
+    url = "https://api.darksky.net/forecast/{}/{},{}".format(DARK_SKY_API_KEY, latitude, longitude)
+
+    response = requests.get(url)
+    data = response.json()
+
+    current_weather = data['currently']
+    summary = current_weather['summary'].lower()
+    temperature = current_weather['temperature']
+
+    message = "Currently, it's {} degrees outside. There's also {}".format(temperature, summary)
+    print(message)
+
+
+if __name__ == "__main__":
+    get_weather(latitude = 37.8267, longitude = -122.4233)
